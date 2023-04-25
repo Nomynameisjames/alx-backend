e49a6a239fba9ecf609db5d586fa7cf8a26e7230
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-25 19:49:57 +0000

    creating flask app instance

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""import files """
 from flask import Flask, render_template
 
 """
@@ -7,10 +8,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     '''flask route index.html'''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

4baa4352be20433607d899447c55609c8ec5a1a7
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-25 21:53:23 +0000

    setting up babel-cfg file

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -29,6 +29,9 @@ def get_locale():
     """
         return best language match based on supported languages
     """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 

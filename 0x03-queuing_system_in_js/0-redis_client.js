da62c74754bb0e794d17788444819f2f09c78adc
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-24 16:44:09 +0000

    node redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,16 @@
+// setting up conneection to redis server
+
+import { createClient } from 'redis';
+
+function redisConnect() {
+  const client = createClient();
+
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+
+};
+
+redisConnect();
